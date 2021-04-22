90ffaf7bb411d830ba9ddc206b0d9f112928a725
Nhan Doan <nhandoan@VN-LW12637-M.local>
2021-04-22 18:01:14 +0700

    change to be microfrontend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./App.css";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter, Switch, Router } from "react-router-dom";
 import LandingPage from "./containers/LandingPage/LandingPage.container";
 import TemplateList from "./containers/TemplateList/TemplateList.container";
+import {createBrowserHistory} from 'history'
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history} >
       <Switch>
         <Route path='/templates' exact component = {TemplateList}/>
         <Route path='/templates/:id' component ={LandingPage} />
         <Route path='/' exact component={LandingPage}/>
       </Switch>
 
-    </BrowserRouter>
+    </Router>
   );
 }
 
